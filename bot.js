1fc929a03dea8c1e19bdfb4c32a31ff89ba20a3d
cleanse0 <54558523+cleanse0@users.noreply.github.com>
2019-08-27 01:16:36 +0300

    Awootism
    AwootismAwootism

---
+++ bot.js
@@ -0,0 +1,121 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'bot commands') {
+    msg.channel.sendMessage('Cleanse bot commands; sa, ananı sikim, aram, oç, lol, çok gizli komut, polat alemdar, saldır oğlum, yiit, insta, naber, sg, ok, ayak, kocacım, iyi geceler, günaydın, selamın aleyküm, tek arkadasım sensin, gariban, asd, selam, bayan, acıktım, nie, köylü kızı,');
+  }
+  if (msg.content === 'sa') {
+    msg.channel.sendMessage('GÖKDELEN ÇOKTUR AMA GÖTVEREN DAHA ÇOK... AS YANİ ALEYKÜM SELAM');
+  }
+  if (msg.content === 'ananı sikim') {
+    msg.channel.sendMessage('ne dion orsopu cocugu');
+  }
+  if (msg.content === 'aram') {
+    msg.channel.sendMessage('<3');
+  }
+
+  if (msg.content === 'oç') {
+    msg.channel.sendMessage('duzgun konus');
+  }
+  if (msg.content === 'lol') {
+    msg.channel.sendMessage('lolü sikeyim');
+  }
+  if (msg.content === 'çok gizli komut') {
+    msg.channel.sendMessage('allah sikilir atatürk dövülür muhammed dövülür kuran yırtılır sehitler yakılır vatna bölünür ');
+  }
+  if (msg.content === 'polat alemdar') {
+    msg.channel.sendMessage('polat alemdar vermeyeceğim adamdır');
+  }
+  if (msg.content === 'seri aram') {
+    msg.channel.sendMessage('cabuk aram gelin orospu cocukları @here');
+  }
+  if (msg.content === 'saldır oğlum') {
+    msg.channel.sendMessage('ananın amına teletabinin antenlerini sokar göbeğindeki televizyondan ulusal porno yayını yaparımananı özgürlük heykelinin yanmayan meşalesinde siker şehri duman ederimhollywood bulvarında donla gezen ananın amına topuklu ayakkabı sokayımananı ikiz kulelerinin yedinci katına cıkartır amına uçakla girerim..ananın o dazlak kafasına teflon tavayla vurur sersemletir sikerim.ananın buruşmuş amına tefal ütü basar dümdüz ederim.ananın amına windows 7 kurar mavi ekran hatası verinceye kadar sikerim.ananın amına telefon kablosu sokar paralel hattan bacını skermgardolapta tangasını arayan ananın kafasını dolap kapagına sıkıştırır müjde ar gibi sikerimdağdan inmediği icin yüzme bilmeyen ananı büyük pasificte 1 ton boşalan beyaz balinalarla beraber-- siker olimpiyat yüzme şampiyonasında altın madalya kazandırırımkırmızı eşarp giyip ormanda kırmızı başlıklı kız gibi takılan anana kurt gibi yaklaşır amını param parça ederimmmpikachuuuuuuuuu diye camdan atlayan sipastik ananın amına poke topu fırlatırımananı balbasaurun sarmaşık kırbacıyla dolar pikacuyla kanka olur veririm elektriğiedisonla kanka olur ananı fakir mahallenizde yanmayan sokak direğine bağlar sike sike trafoyu patlatırımhani benim gençliğim nerde diyen orospu cocugu seni.tavşan kostümü giyip fotograf cektirince kendini playboy dergisinde kapak sanan ananın amına evdeki elektrik süpürgesini sokarımananla karşılıklı salsa yaparken piste takla atarak giren orospu cocugu kardeşini götünden sikerim..ananla karşılıklı sikişirken ay çekirdeği cıtlatırım kabuklarını babanın suratına fırlatırımevde göbeğini yere deydirerek sınav cekince kendini atletik sanan abini götünden sikeyim');
+  }
+  if (msg.content === 'yiit') {
+    msg.channel.sendMessage('20 iq ermeni gocmenı');
+  }
+  if (msg.content === 'insta') {
+    msg.channel.sendMessage( 'https://www.instagram.com/cleanse696 sadece güzel kızlar :sunglasses:  :wink: ');
+  }
+  if (msg.content === 'naber') {
+    msg.channel.sendMessage( 'ii');
+  }
+  if (msg.content === 'sg') {
+    msg.channel.sendMessage( 'siktirgitin yer mum diktir  ');
+  }
+  if (msg.content === 'ok') {
+    msg.channel.sendMessage( 'ok deme');
+  }
+  if (msg.content === 'ayak') {
+    msg.channel.sendMessage( ':yum:');
+  }
+  if (msg.content === 'kocacım') {
+    msg.channel.sendMessage( 'kocandan daha iyi sikiyorum dimi seni ');
+  }
+  if (msg.content === 'iyi geceler') {
+    msg.channel.sendMessage( 'ŞİMDİ SANA İYİ GECELER DEMEK İSTERDİM FAKAT :wink::stuck_out_tongue_winking_eye::kissing_heart:..BİLİRİM Kİ ORSBULAR GECE ÇALIŞIR..');
+  }
+  if (msg.content === 'günaydın') {
+    msg.channel.sendMessage( 'ŞİMDİ SANA GÜNAYDIN DEMEK İSTERDİM FAKAT :wink::stuck_out_tongue_winking_eye::kissing_heart:..BİLİRİM Kİ ORSBULAR SABAH UYUR ...');
+  }
+  if (msg.content === 'selamın aleyküm') {
+    msg.channel.sendMessage( 'cami mi lan burası orospu çocuğu ');
+  }
+  if (msg.content === 'tek arkadasım sensin') {
+    msg.channel.sendMessage( 'biliom knka sıkma canını ');
+  }
+  if (msg.content === 'gariban') {
+    msg.channel.sendMessage( 'Ben gariban biriyim baltızların bana bakacaksa ben hemen yarrak hazır ilk önce karını götten sikerim   ');
+  }
+  if (msg.content === 'asd') {
+    msg.channel.sendMessage( 'asdasddas komik orosbu oglu ');
+  }
+  if (msg.content ==='selam' ) {
+    msg.channel.sendMessage( 'as yani alekyüm selam');
+  }
+  if (msg.content === 'bayan') {
+    msg.channel.sendMessage( 'MERHABA muhteşemsinizz SUPER SEKSİ BAYANSIN');
+  }
+
+  if (msg.content === 'acıktım') {
+    msg.channel.sendMessage( 'Merak etme anne tokum ben:wink::hibiscus::kissing_heart::stuck_out_tongue_winking_eye:..bugünde arkadaşlarımdan kazık yedim');
+  }
+
+  if (msg.content ==='nie' ) {
+    msg.channel.sendMessage( 'ananın amından dolayı' );
+  }
+  if (msg.content ==='köylü kızı' ) {
+    msg.channel.sendMessage('Köylü kızı sikmek isterdim,denize beline kadar giren Köylü kızı sikmek isterdim,kot yerine şalvar giyen Köylü kızı sikmek isterdim,dalardım gözlerinin karasına Köylü kızı sikmek isterdim,samanların arasında Köylü kızı sikmek isterdim,ağzı otlu peynir kokanKöylü kızı sikmek isterdim,bahçede bağ bozan Köylü kızı sikmek isterdim,ahırın arkasında Köylü kızı sikmek isterdim, ya da pamuk tarlasında Köylü kızı sikmek isterdim,ama izin vermez daha derine Köylü kızı sikmek isterdim,hem de köy evinde Köylü kızı sikmek isterdim,bahçede dikenleri yolarke Köylü kızı sikmek isterdim,koyunları otlatırken Köylü kızı sikmek isterdim,bir ege dizisinden fırlamış Köylü kızı sikmek isterdim,daha önce arkadan almamış Köylü kızı sikmek isterdim,hem tırpanlı hem oraklı Köylü kızı sikmek isterdim utangaç ama meraklı Köylü kızı sikmek isterdim,hayalleriyle düşleriyle Bahçede kırardı bana fındık çürük dişleriyle Köylü kızı sikmek isterdim,anlamayan modelinden markasından Köylü kızı sikmek isterdim,tarlada pamuk toplarken yanaşıp sinsice arkasından');
+  }
+  if (msg.content ==='' ) {
+    msg.channel.sendMessage( '');
+  }
+  if (msg.content ==='' ) {
+    msg.channel.sendMessage( '');
+  }
+  if (msg.content ==='' ) {
+    msg.channel.sendMessage( '');
+  }
+  if (msg.content ==='' ) {
+    msg.channel.sendMessage( '');
+  }
+  if (msg.content ==='' ) {
+    msg.channel.sendMessage( '');
+  }
+  if (msg.content ==='' ) {
+    msg.channel.sendMessage( '');
+  }
+
+
+
+
+
+});
+
+client.login('NjE1MTc4MTU4OTAzMjYzMjgw.XWQ6sQ.LrKbk4Xi5psOlnXztx51mcRRh_E');
